edf0c3b3403bd194776088ea87093014d8525f64
AlexisLouisfert <gillale65@gmail.com>
2025-03-27 15:03:28 +0100

    exo4
    oui

--- exo4/index.js
+++ exo4/index.js
@@ -1,3 +1,8 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
-    return 'Hello from Lambda!';
+const params = { Bucket: 'fzfzepjzoefiezfz191fz', Key: 'exotest.txt' };
+const data = await s3.getObject(params).promise();
+return data.Body.toString();
 }
